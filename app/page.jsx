83de5e9b1222c798e8de1ce17e9b5712aa32807a
Domun <zwolinskid1@gmail.com>
2023-02-09 18:31:09 +0100

    Update 03
    - added new font
    - added 4 images
    - added header
    - added popup

--- app/page.jsx
+++ app/page.jsx
@@ -1,16 +1,59 @@
 import Image from 'next/image'
-import { Roboto } from '@next/font/google'
+import { Inter } from '@next/font/google'
 import Link from 'next/link'
 
-const roboto = Roboto({
-  weight: '400',
-  subsets: ['latin-ext'],
+const inter = Inter({
+  subsets: ['latin'],
+  weight: '500',
 })
 
 export default function Home() {
   return (
-    <div className={roboto.className}>
-      <p>Test</p>
+    <div className={inter.className}>
+      <header>
+        <div className="header-cointeiner">
+          <div className="popup">
+            <Image src="/star.png" alt="" width="20" height="19" />
+            <p>
+              <Link href="#">A custom notification goes here</Link>
+            </p>
+          </div>
+          <div className="nav">
+            <div className="logo">
+              <Image src="/logo.png" alt="logo" width="126" height="21" />
+            </div>
+            <div className="nav-position">
+              <nav>
+                <div className="nav-bar">
+                  <ul>
+                    <li>
+                      <Link href="#">
+                        Products
+                        <Image
+                          src="/arrow-down.png"
+                          alt=""
+                          width="16"
+                          height="16"
+                        />
+                      </Link>
+                    </li>
+                    <li>
+                      <Link href="#">Company</Link>
+                    </li>
+                    <li>
+                      <Link href="#">Industries</Link>
+                    </li>
+                  </ul>
+                </div>
+              </nav>
+              <button className="nav-button" href="#">
+                Contact sales
+                <Image src="/arrow-right.png" alt="" width="16" height="16" />
+              </button>
+            </div>
+          </div>
+        </div>
+      </header>
     </div>
   )
 }

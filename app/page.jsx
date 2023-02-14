14e495b9337b01937cc88a8455834381f97a7f73
Domun <zwolinskid1@gmail.com>
2023-02-14 20:46:34 +0100

    Update 06
    - added new icon
    - added testimonials
    - added cards-section

--- app/page.jsx
+++ app/page.jsx
@@ -86,6 +86,81 @@ export default function Home() {
             />
           </div>
         </section>
+        <section className="testimonials">
+          <div className="testimonials-text-box">
+            <p>
+              Used by over <span>50+ milion</span> users worldwide
+              <br /> and trusted by top-tier companies.
+            </p>
+          </div>
+          <div className="testimonials-icons">
+            <Image src="/forbes.png" alt="forbes" width="70" height="19" />
+            <Image src="/fortune.png" alt="fortune" width="80" height="19" />
+            <Image src="/tc.png" alt="tc" width="47" height="23" />
+            <Image src="/wsj.png" alt="wsj" width="52" height="28" />
+            <Image src="/ft.png" alt="ft" width="91" height="28" />
+            <Image src="/cnbc.png" alt="cnbc" width="42" height="32" />
+          </div>
+        </section>
+        <section className="cards-section">
+          <div className="cards-section-text-box">
+            <p>Solutions for every factor</p>
+            <h1>
+              Software architecture, design and
+              <br /> implementation for any industry🔥.
+            </h1>
+          </div>
+          <div className="cards-section-cointeiner-card">
+            <div className="crad">
+              <div>
+                <div className="frame">
+                  <Image src="/qube.png" alt="" width="22" height="22" />
+                </div>
+                <h2>Custom software development</h2>
+              </div>
+              <p>
+                We design and build custom software applications to meet the
+                specific needs of our clients.
+              </p>
+            </div>
+            <div className="crad">
+              <div>
+                <div className="frame">
+                  <Image src="/vector.png" alt="" width="22" height="21" />
+                </div>
+                <h2>Business consulting</h2>
+              </div>
+              <p>
+                Assisting other companies in improving business strategies and
+                operations
+              </p>
+            </div>
+            <div className="crad">
+              <div>
+                <div className="frame">
+                  <Image src="/eath.png" alt="" width="19" height="19" />
+                </div>
+                <h2>Web development services</h2>
+              </div>
+              <p>
+                Creating and designing functional websites for businesses or
+                individuals.
+              </p>
+            </div>
+            <div className="crad">
+              <div>
+                <div className="frame">
+                  <Image src="/qubemore.png" alt="" width="17" height="17" />
+                </div>
+                <h2>Application maintenance and support</h2>
+              </div>
+              <p>
+                We ensure software applications run smoothly and efficiently
+                through ongoing support and maintenance
+              </p>
+            </div>
+          </div>
+        </section>
       </main>
     </div>
   )
